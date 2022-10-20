1fd20e99a83b25f58be149af9482236cf11250e7
nicholas silva santos <doctorredac@gmail.com>
2022-10-19 21:42:00 -0300

    Exercices

---
+++ coffe.c
@@ -0,0 +1,26 @@
+// N = ESTUDANTES
+// D = MILILITROS POR ESTUDANTES
+// L = LITROS DA GARRAFA
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int n,l,d;
+    printf("Insert the quantity of people: ");
+    scanf("%4i", &n);
+    printf("Insert the coffe pot litters: ");
+    scanf("%4i", &l);
+    printf("Insert the quantity of coffe in mililiters, per person: ");
+    scanf("%4i", &d);
+
+    float calc = (n*d)/1000;
+    int output = l;
+    printf("%i", output);
+    while(output < calc) {
+        output+=l;
+    };
+    printf("%i", output);
+    return 0;
+};
--- searchString.c
+++ searchString.c
@@ -3,9 +3,14 @@
 
 
 void main() {
-    char s0[] = "Low Roar - Anything You Need";
-    char s1[] = "Roar";
-    
+    char s0[30];
+    char s1[10];
+
+    printf("Insert a phrase: ");
+    fgets(s0, 29, stdin);
+    printf("Insert a word to search for, in the phrase: ");
+    fgets(s1, 9, stdin);
+
     if(strstr(s0, s1))
-        printf("I found the %s in %s", s1, s0);
+        printf("The phrase %scontains %s", &s1, &s0);
 }
